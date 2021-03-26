8f467dc4c68c52f21d599aae315b483b9abaf01b
Your Username <you@example.com>
2021-03-26 17:01:39 -0400

    commit tree 2441fcde9c3a8a9b6a8f5981f19dc1d8b499817b

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
-class Post < ActiveRecord::Base
+class Post < ActiveRecord::Base 
+    validates :title, presence: true 
+    validates :content, length: { minimum: 250 } 
+   validates :summary, length: {maximum: 250} 
+#    validates :category, acceptance: true, message: "Non-Fiction" 
+#    validates :category, acceptance: true, message "Fiction"
+    validates :category, inclusion: {in: ["Non-Fiction", "Fiction"]} 
+    validate :clickbait
+    def clickbait 
+        if [/Won't Believe/i, /Secret/i, /Top [0-9]/i, /Guess/i].none? {|pattern| pattern.match(title)} 
+            self.errors[:title] << "Oh must be clickbait" 
+        end 
+
+    end 
 end
